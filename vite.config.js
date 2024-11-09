fef551e1d40b24a71dfd558ff507a4824f6895fe
ser4ph22 <seanpalomo22@gmail.com>
2024-11-09 10:54:10 -1000

    build: Prepare for Azure deployment

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
     sourcemap: true,
-    rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, 'index.html'),
-      },
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true
+      }
     }
   },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
   server: {
-    port: 3000
+    port: 3000,
+    open: true
   }
 })
