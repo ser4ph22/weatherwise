9d6a8ed75935f0307527d6de8f10c385a19f785c
ser4ph22 <seanpalomo22@gmail.com>
2024-11-09 11:43:16 -1000

    perf: Implement aggressive build optimization

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,64 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import viteImagemin from 'vite-plugin-imagemin'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteImagemin({
+      gifsicle: {
+        optimizationLevel: 7,
+        interlaced: false
+      },
+      optipng: {
+        optimizationLevel: 7
+      },
+      mozjpeg: {
+        quality: 60
+      },
+      pngquant: {
+        quality: [0.7, 0.8],
+        speed: 4
+      },
+      webp: {
+        quality: 70
+      },
+      svgo: {
+        multipass: true,
+        plugins: [
+          {
+            name: 'removeViewBox',
+            active: false
+          }
+        ]
+      }
+    })
+  ],
   build: {
     outDir: 'build',
-    sourcemap: false, // Disable sourcemaps for production
+    sourcemap: false,
     minify: 'terser',
     terserOptions: {
       compress: {
         drop_console: true,
-        drop_debugger: true
+        drop_debugger: true,
+        pure_funcs: ['console.log', 'console.info', 'console.debug', 'console.warn']
       }
     },
     rollupOptions: {
       output: {
         manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom'],
-        }
+          vendor: ['react', 'react-dom'],
+          router: ['react-router-dom'],
+          utils: ['prop-types', '@microsoft/applicationinsights-web']
+        },
+        chunkFileNames: 'assets/[hash].js',
+        entryFileNames: 'assets/[hash].js',
+        assetFileNames: 'assets/[hash][extname]'
       }
-    }
-  },
-  server: {
-    port: 3000,
-    open: true
+    },
+    chunkSizeWarningLimit: 1000,
+    cssCodeSplit: true,
+    reportCompressedSize: false
   }
 })
