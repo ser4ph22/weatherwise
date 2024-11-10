ef8122226b3c589d0039e7d45cfa8ee996e100da
ser4ph22 <seanpalomo22@gmail.com>
2024-11-10 03:13:52 -1000

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,49 +1,38 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { visualizer } from 'rollup-plugin-visualizer';
+import { terser } from 'rollup-plugin-terser';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    visualizer({
+      filename: './dist/stats.html',
+      open: false,
+      gzipSize: true,
+    }),
+  ],
   build: {
     outDir: 'dist',
-    minify: 'terser',
     sourcemap: false,
+    minify: 'terser',
     terserOptions: {
       compress: {
         drop_console: true,
         drop_debugger: true,
-        pure_funcs: ['console.log'],
-        passes: 2
       },
-      format: {
-        comments: false
-      },
-      mangle: {
-        toplevel: true
-      }
     },
     rollupOptions: {
       output: {
         manualChunks: {
-          'react-vendor': ['react', 'react-dom', 'react-router-dom'],
-          'ui-vendor': ['@headlessui/react', '@heroicons/react', 'lucide-react'],
-          'chart-vendor': ['recharts'],
-          'utils': ['axios', '@tanstack/react-query']
+          'react-vendor': ['react', 'react-dom'],
+          'router-vendor': ['react-router-dom'],
         },
-        inlineDynamicImports: false,
-        chunkFileNames: 'assets/[hash:8].js',
-        entryFileNames: 'assets/[hash:8].js',
-        assetFileNames: 'assets/[hash:8][extname]'
-      }
+      },
     },
-    target: 'es2018',
-    cssCodeSplit: false,
-    assetsInlineLimit: 4096,
-    chunkSizeWarningLimit: 500,
-    emptyOutDir: true,
-    reportCompressedSize: false
+    chunkSizeWarningLimit: 1000,
   },
   optimizeDeps: {
-    include: ['react', 'react-dom', 'react-router-dom']
-  }
+    include: ['react', 'react-dom', 'react-router-dom'],
+  },
 });
